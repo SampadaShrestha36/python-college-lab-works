bca3069a920e3828eca569fcc88fec0deeae1009
Sampada Shrestha <161410290+SampadaShrestha36@users.noreply.github.com>
2024-09-06 08:32:12 +0545

    Add files via upload

---
+++ lab3/Problem12.py
@@ -0,0 +1,20 @@
+'''Write a Python program to construct the following pattern, using a nested for loop. 
+*  
+* *  
+* * *  
+* * * *  
+* * * * *  
+* * * *  
+* * *  
+* *  
+*
+'''
+for i in range(1,10):
+    if i<=5:
+        for j in range(0,i):
+            print('*',end="")
+        print()
+    else:
+        for j in range(i,10):
+            print('*',end="")
+        print()
